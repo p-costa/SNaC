7eb3d8aa9d9b2febb9944600ed3bc5f18dc2639f
Pedro Costa <p.simoes.costa@gmail.com>
2020-06-26 11:28:53 +0000

    fixed record length so that it works on intel and gnu compilers

--- src/initgrid.f90
+++ src/initgrid.f90
@@ -97,9 +97,10 @@ module mod_initgrid
     character(len=*), intent(in) :: fname
     integer         , intent(in) :: lo_g,hi_g
     real(rp)        , intent(in), dimension(lo_g-1:) :: rf_g,rc_g,drf_g,drc_g
-    integer :: iunit,q,ng
-    ng = hi_g-lo_g+1
-    open(newunit=iunit,file=trim(fname)//'.bin',status='replace',access='direct',recl=4*ng*storage_size(1._rp)/8)
+    integer :: iunit,q,reclen
+    !
+    inquire(iolength=reclen) rf_g(lo_g:hi_g),rc_g(lo_g:hi_g),drf_g(lo_g:hi_g),drc_g(lo_g:hi_g)
+    open(newunit=iunit,file=trim(fname)//'.bin',status='replace',access='direct',recl=reclen)
     write(iunit,rec=1) rf_g(lo_g:hi_g),rc_g(lo_g:hi_g),drf_g(lo_g:hi_g),drc_g(lo_g:hi_g)
     close(iunit)
     open(newunit=iunit,status='replace',file=trim(fname)//'.out')
