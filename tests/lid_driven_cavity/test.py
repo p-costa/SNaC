1621901644f0c120f70cc83fe96e918be2d14a8e
Pedro Costa <p.simoes.costa@gmail.com>
2020-08-10 16:38:08 +0000

    testing 2D lid-driven cavity flows in all directions, i.e. in xy, yz and zx planes

--- tests/lid_driven_cavity/test.py
+++ tests/lid_driven_cavity/test.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 def test_ldc():
     import numpy as np
+    import os
     from read_single_field_binary import read_single_field_binary
-    data,xp,yp,zp,xu,yv,zw = read_single_field_binary("vey_fld_0001500.bin",np.array([1,1,1]))
     data_ref = np.loadtxt("data_ldc_re1000.txt")
-    islice = int(np.size(data[0,0,:])/2)
-    np.testing.assert_allclose(data[0,islice,:], data_ref[:,1], rtol=1e-7, atol=0)
+    if "data_x" in os.getcwd():
+        data,xp,yp,zp,xu,yv,zw = read_single_field_binary("vey_fld_0001500.bin",np.array([1,1,1]))
+        islice = int(np.size(data[0,0,:])/2)
+        np.testing.assert_allclose(data[0,islice,:], data_ref[:,1], rtol=1e-7, atol=0)
+    if "data_y" in os.getcwd():
+        data,xp,yp,zp,xu,yv,zw = read_single_field_binary("vex_fld_0001500.bin",np.array([1,1,1]))
+        islice = int(np.size(data[0,0,:])/2)
+        np.testing.assert_allclose(data[islice,0,:], data_ref[:,1], rtol=1e-7, atol=0)
+    if "data_z" in os.getcwd():
+        data,xp,yp,zp,xu,yv,zw = read_single_field_binary("vex_fld_0001500.bin",np.array([1,1,1]))
+        islice = int(np.size(data[0,:,0])/2)
+        np.testing.assert_allclose(data[islice,:,0], data_ref[:,1], rtol=1e-7, atol=0)
 if __name__ == "__main__":
     test_ldc()
     print("Passed!")
--- tests/lid_driven_cavity/testit.sh
+++ tests/lid_driven_cavity/testit.sh
@@ -9,12 +9,13 @@ rm -rf $RUNDIR
 echo "Compiling ..."
 sleep 2
 cp $TESTDIR/Makefile $SRCDIR && cd $SRCDIR && make clean && make -j run
-cp $TESTDIR/dns.in $RUNDIR && cd $RUNDIR
-echo "Running SNaC..."
-sleep 2
-mpirun -n 4 ./${EXEC}
-cp $TESTDIR/*.* data/ && cp $UTILSDIR/read_binary_data/python/read_single_field_binary.py $RUNDIR/data/ && cd $RUNDIR/data/
-echo "Running test..."
-sleep 2
-pytest test.py
+for i in "_x" "_y" "_z"; do
+  cp $TESTDIR/dns${i}.in $RUNDIR/dns.in && cd $RUNDIR && mkdir -p $RUNDIR/data
+  echo "Running SNaC..."; sleep 2
+  mpirun -n 4 ./${EXEC}
+  mkdir -p $RUNDIR/data${i} && mv $RUNDIR/data/* $RUNDIR/data${i} && cd $RUNDIR/data${i}
+  cp $TESTDIR/*.* $RUNDIR/data${i} && cp $UTILSDIR/read_binary_data/python/read_single_field_binary.py $RUNDIR/data${i} && cd $RUNDIR/data${i}
+  echo "Running test..."; sleep 2
+  pytest test.py
+done
 rm -rf $RUNDIR
