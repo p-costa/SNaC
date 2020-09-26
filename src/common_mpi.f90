5608c3382ad09c5fb7f80276c0ff4b1d0e2e88d5
Pedro Costa <p.simoes.costa@gmail.com>
2020-09-26 12:47:07 +0000

    only import necessary parameters

--- src/common_mpi.f90
+++ src/common_mpi.f90
@@ -1,5 +1,4 @@
 module mod_common_mpi
-  use mpi
   implicit none
   integer :: myid,ierr,comm_cart
 end module mod_common_mpi
--- src/types.f90
+++ src/types.f90
@@ -1,16 +1,16 @@
 module mod_types
-  use, intrinsic :: iso_fortran_env, stdin  => input_unit , &
-                                     stdout => output_unit, &
-                                     stderr => error_unit
-  use, intrinsic :: iso_fortran_env, sp => real32, &
-                                     dp => real64
-  use            :: mpi            , MPI_REAL_DP => MPI_REAL, &
-                                     MPI_REAL_SP => MPI_DOUBLE_PRECISION
+  use, intrinsic :: iso_fortran_env, only: stdin  => input_unit , &
+                                           stdout => output_unit, &
+                                           stderr => error_unit
+  use, intrinsic :: iso_fortran_env, only: sp => real32, &
+                                           dp => real64
+  use            :: mpi            , only: MPI_REAL_DP => MPI_REAL, &
+                                           MPI_REAL_SP => MPI_DOUBLE_PRECISION
 #ifdef _SINGLE_PRECISION
-  use, intrinsic :: iso_fortran_env, rp          => real32
-  use            :: mpi            , MPI_REAL_RP => MPI_REAL
+  use, intrinsic :: iso_fortran_env, only: rp          => real32
+  use            :: mpi            , only: MPI_REAL_RP => MPI_REAL
 #else
-  use, intrinsic :: iso_fortran_env, rp          => real64
-  use            :: mpi            , MPI_REAL_RP => MPI_DOUBLE_PRECISION
+  use, intrinsic :: iso_fortran_env, only: rp          => real64
+  use            :: mpi            , only: MPI_REAL_RP => MPI_DOUBLE_PRECISION
 #endif
 end module mod_types
