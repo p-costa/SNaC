5760bd8903f7245bfaf923668b73887255a034ab
Pedro Costa <p.simoes.costa@gmail.com>
2021-02-16 10:35:30 +0000

    fixed integer precision for computation of the expected file size
    it was giving problems with the Intel compilers. thanks @arashalizadbanaei !

--- src/load.f90
+++ src/load.f90
@@ -32,8 +32,8 @@ module mod_load
       ! check file size first
       !
       call MPI_FILE_GET_SIZE(fh,filesize)
-      good = (product(ng)*4+2)*storage_size(1._rp)/8
-      if(present(po)) good = good + product(ng)*storage_size(1._rp)/8
+      good = (product(ng)*4+2)*int(storage_size(1._rp)/8,MPI_OFFSET_KIND)
+      if(present(po)) good = good + product(ng)*int(storage_size(1._rp)/8,MPI_OFFSET_KIND)
       if(filesize /= good) then
         if(myid_block == 0) write(stderr,*) ''
         if(myid_block == 0) write(stderr,*) '*** Simulation aborted due a checkpoint file with incorrect size ***'
