cc0a8311e19c64168630678e40c2c141326711be
Pedro Costa <p.simoes.costa@gmail.com>
2021-05-10 10:00:53 +0000

    fixed openmp compilation

--- src/rk.f90
+++ src/rk.f90
@@ -60,6 +60,9 @@ module mod_rk
     call momz_a(lo,hi,dxf,dyf,dzc,dzf,u,v,w,dwdtrk)
     !$OMP PARALLEL DO DEFAULT(none) &
     !$OMP PRIVATE(i,j,k) &
+#ifdef _IMPDIFF
+    !$OMP SHARED(factor12,dudtrkd,dvdtrkd,dwdtrkd) &
+#endif
     !$OMP SHARED(lo,hi,factor1,factor2,u,v,w,up,vp,wp,dudtrk,dvdtrk,dwdtrk,dudtrko,dvdtrko,dwdtrko)
     do k=lo(3),hi(3)
       do j=lo(2),hi(2)
