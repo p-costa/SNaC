953d2c1bea318ba4a34851bfba642b86f611b2fa
Pedro Costa <p.simoes.costa@gmail.com>
2021-05-24 18:50:10 +0000

    swapped MPI types definition
    not used yet in the code

--- src/types.f90
+++ src/types.f90
@@ -4,8 +4,8 @@ module mod_types
                                            stderr => error_unit
   use, intrinsic :: iso_fortran_env, only: sp => real32, &
                                            dp => real64
-  use            :: mpi            , only: MPI_REAL_DP => MPI_REAL, &
-                                           MPI_REAL_SP => MPI_DOUBLE_PRECISION
+  use            :: mpi            , only: MPI_REAL_SP => MPI_REAL, &
+                                           MPI_REAL_DP => MPI_DOUBLE_PRECISION
 #ifdef _SINGLE_PRECISION
   use, intrinsic :: iso_fortran_env, only: rp          => real32
   use            :: mpi            , only: MPI_REAL_RP => MPI_REAL
