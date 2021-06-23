1ece138b608d67477142d72fea839596c6075caa
Pedro Costa <p.simoes.costa@gmail.com>
2021-06-23 14:52:05 +0000

    fixed automated tests

--- tests/lid_driven_cavity/testit.sh
+++ tests/lid_driven_cavity/testit.sh
@@ -14,7 +14,7 @@ cp $TESTDIR/dns.in $RUNDIR && cd $RUNDIR
 rm -rf $RUNDIR/geo && cp -r $TESTDIR/geo $RUNDIR/geo
 echo "Running SNaC..."
 sleep 2
-mpirun -n 4 ./${EXEC}
+mpirun -n 4 --oversubscribe ./${EXEC}
 cp $TESTDIR/*.* data/ && cp $UTILSDIR/read_binary_data/python/read_single_field_binary.py $RUNDIR/data/ && cd $RUNDIR/data/
 echo "Running test..."
 sleep 2
