648e9f2aaf3e7d01269af4104986fa28aedb8e3c
Pedro Costa <p.simoes.costa@gmail.com>
2021-10-14 10:46:06 +0000

    more portable implementation of types

--- src/types.f90
+++ src/types.f90
@@ -2,16 +2,19 @@ module mod_types
   use, intrinsic :: iso_fortran_env, only: stdin  => input_unit , &
                                            stdout => output_unit, &
                                            stderr => error_unit
-  use, intrinsic :: iso_fortran_env, only: sp => real32, &
-                                           dp => real64, &
-                                           i8 => int64
   use            :: mpi_f08        , only: MPI_REAL_SP => MPI_REAL, &
                                            MPI_REAL_DP => MPI_DOUBLE_PRECISION
 #ifdef _SINGLE_PRECISION
-  use, intrinsic :: iso_fortran_env, only: rp          => real32
   use               mpi_f08        , only: MPI_REAL_RP => MPI_REAL
 #else
-  use, intrinsic :: iso_fortran_env, only: rp          => real64
   use               mpi_f08        , only: MPI_REAL_RP => MPI_DOUBLE_PRECISION
+#endif
+  integer, parameter :: sp = selected_real_kind(6 , 37), &
+                        dp = selected_real_kind(15,307), &
+                        i8 = selected_int_kind(18)
+#ifdef _SINGLE_PRECISION
+  integer, parameter :: rp = sp
+#else
+  integer, parameter :: rp = dp
 #endif
 end module mod_types
